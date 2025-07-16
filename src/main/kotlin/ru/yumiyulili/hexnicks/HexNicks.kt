bb644d64257cf0a3f39b3201d9446695b40ddecf
yumiyulili <masha.ava04@gmail.com>
2025-07-16 15:08:15 +0300

    update

--- src/main/kotlin/ru/yumiyulili/hexnicks/HexNicks.kt
+++ src/main/kotlin/ru/yumiyulili/hexnicks/HexNicks.kt
@@ -1,14 +1,30 @@
 package ru.yumiyulili.hexnicks
 
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 import ru.destroy.RustWrapper
 
-class HexNicks : JavaPlugin() {
+
+class HexNicks : JavaPlugin() , Listener {
 
     override fun onEnable() {
-        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this,this)
 
     }
+
+    @EventHandler
+    fun name(e: PlayerJoinEvent) {
+        var user = e.player.name
+        var color = Color.color(RustWrapper.hexColorOf(user));
+        var player = e.player
+        player.setDisplayName(color.plus(user))
+        player.setPlayerListName(color.plus(user))
+    }
+
 
     override fun onDisable() {
         // Plugin shutdown logic
