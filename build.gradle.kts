997c84f6c5c68992eeee257db0c44ba90ed89d37
Destroy <emptycsgo@mail.ru>
2025-07-16 10:08:57 +0300

    Some changes..

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(project(":standalone"))
 }
 
 tasks {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "hexnicks"
+
+include("standalone")
